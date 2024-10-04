8b4597209f285c5c76c8623a24c7c1eb158e4606
smojescikdev <s.mojescik.dev@gmail.com>
2024-10-04 09:38:34 +0200

    Initial commit

---
+++ src/main/java/com/smojdev/DigitalRecruitingPlattform/DigitalRecruitingPlattformApplication.java
@@ -0,0 +1,13 @@
+package com.smojdev.DigitalRecruitingPlattform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalRecruitingPlattformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalRecruitingPlattformApplication.class, args);
+	}
+
+}
